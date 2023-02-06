a05a01836b29a56068392108593bd9b8ac3d893c
Alejandro Villarreal <aleproyectos0809@gmail.com>
2023-02-06 16:42:44 -0400

    Tercer commit

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import "../Products/Products.scss";
 
 const Products = () => {
-  return <div className="products">Products</div>;
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Categorías de Producto</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">Zapatos</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filtrar por precio</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Zapatos</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Ordenar por</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3} />
+            <label htmlFor="3">Zapatos</label>
+          </div>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default Products;
